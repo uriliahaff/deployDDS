9be4563255ae529c3382e5144876eccea6eb79b1
uriliahaff <liahaffuriel@gmail.com>
2023-12-09 14:57:45 -0300

    configs

--- src/main/java/sever/App.java
+++ src/main/java/sever/App.java
@@ -1,8 +1,21 @@
 package sever;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
-    public static void main(String[] args) {
-        Server.init();
-    }
+        public static EntityManagerFactory entityManagerFactory;
+        public static void main(String[] args) {
+            Map<String, String> env = System.getenv();
+            for (String string : env.keySet()) {
+                System.out.println(string + ": " + env.get(string));
+            }
+
+
+            Server.init();
+        }
+
 }
